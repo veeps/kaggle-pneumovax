b25a2c735767f30644e375c2359be35c6cd284c1
Vivian Peng <vpeng@Admins-MacBook-Pro-3.local>
2016-07-01 18:02:45 +0200

    basic GLM test

--- baseline.R
+++ baseline.R
@@ -1,26 +1,32 @@
+setwd("~/git/kaggle-pneumovax")
+
+#load libraries
 library(lambda.tools)
 library(futile.logger)
-setwd("~/Downloads/CUNY")
 library(randomForest)
 
-df <- read.csv('training.csv', stringsAsFactors=FALSE)
-head(df)
-df$grouping <- as.factor(df$grouping)
-df$in.epi <- as.factor(df$in.epi)
-vars <- colnames(df)[! colnames(df) %in% c('code','country','year')]
-model <- randomForest(deaths.per.1k ~ ., df[,vars], na.action=na.omit)
+#read in data
+pneumo.train <- read.csv('Data/training.csv', stringsAsFactors=FALSE)
+pneumo.train$grouping <- as.factor(pneumo.train$grouping)
+pneumo.train$in.epi <- as.factor(pneumo.train$in.epi)
+vars <- colnames(pneumo.train)[! colnames(pneumo.train) %in% c('code','country','year')]
 
-test <- read.csv('testing.csv', stringsAsFactors=FALSE)
+#build linear model
+model <- glm(deaths.per.1k ~ total.usd.2013 + PCV10.Price + PCV13.Price + 
+               gni.2013 + sanitation.2013 + birth.rate.2013, data = train)
+
+
+#read in test dta
+test <- read.csv('Data/testing.csv', stringsAsFactors=FALSE)
 test$grouping <- factor(test$grouping, levels=levels(df$grouping))
 test$in.epi <- factor(test$in.epi, levels=levels(df$in.epi))
 vars <- colnames(test)[! colnames(test) %in% c('code','country','year')]
-test <- fold(vars[!vars %in% c('grouping','in.epi','pct.pop.vaccinated')], 
-  function(v,acc) { acc[,v] <- as.numeric(acc[,v]); acc }, test)
+
+
+#predict deaths
 pred <- predict(model, newdata=test[,vars])
-
-?randomForest
-
 out <- data.frame(Id=test$code, Prediction=pred)
 out$Prediction[is.na(out$Prediction)] <- 0
-# write.csv(out, 'submission.csv', row.names=FALSE, quote=FALSE)
+
+write.csv(out, 'submission.csv', row.names=FALSE, quote=FALSE)
 
